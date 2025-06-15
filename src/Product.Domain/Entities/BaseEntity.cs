d6d0bed72da67704aa8994c3b32c1d5baec76227
tridoann <tridoanngoc@outlook.com>
2025-06-15 22:46:06 +0700

    change docker file

--- src/Product.Domain/Entities/BaseEntity.cs
+++ src/Product.Domain/Entities/BaseEntity.cs
@@ -3,7 +3,7 @@ namespace Product.Domain.Entities;
 public abstract class BaseEntity<TKey>
     where TKey : notnull
 {
-    public TKey Id { get; set; }
+    public TKey Id { get; set; } = default!;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
